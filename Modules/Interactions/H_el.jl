4749d5d8c7da139095f01319fea3c4f888fe560e
tuhmuhfuhuh <tmfhirsch@gmail.com>
2020-12-28 16:41:20 +1100

    fixed comment

--- Modules/Interactions/H_el.jl
+++ Modules/Interactions/H_el.jl
@@ -55,7 +55,7 @@ function H_el_coeffs(bra::asym_αβlml_ket,ket::asym_αβlml_ket)
     return (C¹,C³,C⁵)
 end
 
-""" Radial function: (c₁,c₂,c₃) -> c₁*¹V(R)+c₂*²V(R)+c₃*³V(R)"""
+""" Radial function: (c₁,c₃,c₅) -> c₁*¹V(R)+c₃*⁵V(R)+c₃*⁵V(R)"""
 function H_el_radial(cs::Tuple{Float64,Float64,Float64}, R::Unitful.Length)
     return cs[1]*Singlet(R) + cs[2]*Triplet(R) + cs[3]*Quintet(R)
 end
